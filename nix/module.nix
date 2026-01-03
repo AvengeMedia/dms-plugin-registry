9034dbbcaca14693abde4f009bf18ce1f254272b
Parthiv Seetharaman <parthivs@myrdd.info>
2026-01-02 18:09:34 -0800

    feat: support nixpkgs dms shell module

--- nix/module.nix
+++ nix/module.nix
@@ -1,10 +1,26 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, options, ... }:
 let
   plugins = import ./default.nix { inherit pkgs; };
-in
-{
-  programs.dankMaterialShell.plugins = lib.mapAttrs (name: plugin: {
+
+  pluginsConfig = lib.mapAttrs (name: plugin: {
     enable = lib.mkDefault false;
     src = plugin;
   }) plugins;
-}
+
+  moduleForStableFlake = lib.optionalAttrs (options ? programs.dankMaterialShell.plugins) {
+    programs.dankMaterialShell.plugins = pluginsConfig;
+  };
+
+  moduleForFlake = lib.optionalAttrs (options ? programs.dank-material-shell.plugins) {
+    programs.dank-material-shell.plugins = pluginsConfig;
+  };
+
+  moduleForNixpkgs = lib.optionalAttrs (options ? programs.dms-shell.plugins) {
+    programs.dms-shell.plugins = pluginsConfig;
+  };
+in
+lib.mkMerge [
+  moduleForStableFlake
+  moduleForFlake
+  moduleForNixpkgs
+]
