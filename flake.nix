758264299aef92e1e4b04ff4f82b9904c6f15b06
faukah <fau@faukah.com>
2026-05-28 22:10:59 +0200

    nix: format flake using nixfmt

--- flake.nix
+++ flake.nix
@@ -1,17 +1,25 @@
 {
 
   inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
     in
     {
       checks = self.packages;
-      packages = lib.genAttrs systems (system:
+      packages = lib.genAttrs systems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-        in import ./nix/default.nix { inherit pkgs; }
+        in
+        import ./nix/default.nix { inherit pkgs; }
       );
       nixosModules.dms-plugin-registry = ./nix/module.nix;
       nixosModules.default = self.nixosModules.dms-plugin-registry;
